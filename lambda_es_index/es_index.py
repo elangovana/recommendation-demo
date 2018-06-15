5f10d7bee9111d606d43771c58ed78a886fa38f2
aeg <aeg@amazon.com>
2018-06-15 21:01:16 +1000

    Add index and buildspec

---
+++ lambda_es_index/es_index.py
@@ -0,0 +1,13 @@
+def connectES(esEndPoint):
+ print ('Connecting to the ES Endpoint {0}'.format(esEndPoint))
+ try:
+  esClient = Elasticsearch(
+   hosts=[{'host': esEndPoint, 'port': 443}],
+   use_ssl=True,
+   verify_certs=True,
+   connection_class=RequestsHttpConnection)
+  return esClient
+ except Exception as E:
+  print("Unable to connect to {0}".format(esEndPoint))
+  print(E)
+  exit(3)

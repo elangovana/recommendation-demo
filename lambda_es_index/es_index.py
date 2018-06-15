b97f4f8b3944a52402a63871e3866568adf2cfb3
aeg <aeg@amazon.com>
2018-06-16 00:13:28 +1000

    Fix Lambda index and include ES Domain name

--- lambda_es_index/es_index.py
+++ lambda_es_index/es_index.py
@@ -1,3 +1,13 @@
+from __future__ import print_function
+from pprint import pprint
+import boto3
+import json
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+
+import urllib
+import json
+
+
 def connectES(esEndPoint):
  print ('Connecting to the ES Endpoint {0}'.format(esEndPoint))
  try:
@@ -11,3 +21,37 @@ def connectES(esEndPoint):
   print("Unable to connect to {0}".format(esEndPoint))
   print(E)
   exit(3)
+
+
+
+
+def createIndex(esClient):
+ try:
+  res = esClient.indices.exists('metadata-store')
+  print("Index Exists ... {}".format(res))
+  if res is False:
+   esClient.indices.create('metadata-store', body=indexDoc)
+   return 1
+ except Exception as E:
+  print("Unable to Create Index {0}".format("metadata-store"))
+  print(E)
+  exit(4)
+
+def indexDocElement(esClient, key, response):
+  try:
+   indexObjectKey = key
+   indexcreatedDate = response['LastModified']
+   indexcontent_length = response['ContentLength']
+   indexcontent_type = response['ContentType']
+   indexmetadata = json.dumps(response['Metadata'])
+   retval = esClient.index(index='metadata-store', doc_type='images', body={
+     'createdDate': indexcreatedDate,
+     'objectKey': indexObjectKey,
+     'content_type': indexcontent_type,
+     'content_length': indexcontent_length,
+     'metadata': indexmetadata
+   })
+  except Exception as E:
+    print("Doc not indexed")
+    print("Error: ",E)
+    exit(5)
---
+++ lambda_es_index/lambda_handler.py
@@ -0,0 +1,31 @@
+from __future__ import print_function
+from pprint import pprint
+import boto3
+import json
+from elasticsearch import Elasticsearch, RequestsHttpConnection
+
+import urllib
+import json
+import os
+
+def lambda_handler(event, context):
+   esClient = connectES(os.environ['elasticsearch_domain_name'])
+   createIndex(esClient)
+
+   # Get the object from the event and show its content type
+   bucket = event['Records'][0]['s3']['bucket']['name']
+   key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
+   try:
+     response = s3.get_object(Bucket=bucket, Key=key)
+     print(response)
+     print("KEY: " + key)
+     print("CONTENT TYPE: " + response['ContentType'])
+     print("Metadata : " + json.dumps(response['Metadata']))
+     print("Custom 1: " + response['ResponseMetadata']['HTTPHeaders']['x-amz-meta-custom1'])
+     print("Custom 2: " + response['ResponseMetadata']['HTTPHeaders']['x-amz-meta-custom2'])
+     indexDocElement(esClient,key,response)
+     return response['ContentType']
+   except Exception as e:
+     print(e)
+     print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+     raise e

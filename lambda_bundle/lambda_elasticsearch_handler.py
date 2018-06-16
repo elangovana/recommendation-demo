3e4e35aec184e8c0f09941e3e7b1aafda3202e48
aeg <aeg@amazon.com>
2018-06-16 23:39:36 +1000

    typo

--- lambda_bundle/lambda_elasticsearch_handler.py
+++ lambda_bundle/lambda_elasticsearch_handler.py
@@ -18,7 +18,7 @@ def index_handler(event, context):
    bucket = event['Records'][0]['s3']['bucket']['name']
    key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
    try:
-     indexBulkCsv(esClient, indexName, bucket, key,response)
+     indexBulkCsv(esClient, indexName, bucket, key)
    except Exception as e:
      print(e)
      print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))

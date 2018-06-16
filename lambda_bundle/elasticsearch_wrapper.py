0054844f8217ce7224ab656b4373319c6b48993b
aeg <aeg@amazon.com>
2018-06-17 00:58:03 +1000

    Add elastic search auth

--- lambda_bundle/elasticsearch_wrapper.py
+++ lambda_bundle/elasticsearch_wrapper.py
@@ -10,47 +10,48 @@ import json
 import uuid
 
 
-def connectES(esEndPoint):
- print ('Connecting to the ES Endpoint {0}'.format(esEndPoint))
- try:
-  esClient = Elasticsearch(
-   hosts=[{'host': esEndPoint, 'port': 443}],
-   use_ssl=True,
-   verify_certs=True,
-   connection_class=RequestsHttpConnection)
-  return esClient
- except Exception as E:
-  print("Unable to connect to {0}".format(esEndPoint))
-  print(E)
-  exit(3)
+def connectES(esEndPoint, auth):
+    print('Connecting to the ES Endpoint {0}'.format(esEndPoint))
+    try:
+        esClient = Elasticsearch(
+            hosts=[{'host': esEndPoint, 'port': 443}],
+            use_ssl=True,
+            verify_certs=True,
+            connection_class=RequestsHttpConnection,
+            http_auth=auth)
+        return esClient
+    except Exception as E:
+        print("Unable to connect to {0}".format(esEndPoint))
+        print(E)
+        exit(3)
 
 
 def createIndex(esClient, indexName):
- try:
-  res = esClient.indices.exists(indexName)
-  print("Index Exists ... {}".format(res))
-  if res is False:
-   indexDoc = {
-    "settings" : {
-        "number_of_shards": 1,
-        "number_of_replicas": 0
-    }
-    }
-   esClient.indices.create(indexName, body=indexDoc)
-   return 1
- except Exception as E:
-  print("Unable to Create Index {0}".format(indexName))
-  print(E)
-  exit(4)
+    try:
+        res = esClient.indices.exists(indexName)
+        print("Index Exists ... {}".format(res))
+        if res is False:
+            indexDoc = {
+                "settings": {
+                    "number_of_shards": 1,
+                    "number_of_replicas": 0
+                }
+            }
+            esClient.indices.create(indexName, body=indexDoc)
+            return 1
+    except Exception as E:
+        print("Unable to Create Index {0}".format(indexName))
+        print(E)
+        exit(4)
+
 
 def indexBulkCsv(esClient, indexName, bucket, key):
-  s3_client = boto3.client('s3')
-  tmp_download_file = '/tmp/{}{}'.format(uuid.uuid4(), key)
-  s3_client.download_file(bucket, key, tmp_download_file)
-  with open(tmp_download_file) as f:
-      reader = csv.DictReader(f, encoding='utf-8')
-      for success, info in helpers.parallel_bulk(esClient, reader, thread_count=8, chunk_size=500, index=indexName, doc_type="movies", request_timeout=30):
-          if not success: 
-            print('Doc failed', info) 
-            exit(4)
-
+    s3_client = boto3.client('s3')
+    tmp_download_file = '/tmp/{}{}'.format(uuid.uuid4(), key)
+    s3_client.download_file(bucket, key, tmp_download_file)
+    with open(tmp_download_file) as f:
+        reader = csv.DictReader(f, encoding='utf-8')
+        for success, info in helpers.parallel_bulk(esClient, reader, thread_count=8, chunk_size=500, index=indexName, doc_type="movies", request_timeout=30):
+            if not success:
+                print('Doc failed', info)
+                exit(4)
--- lambda_bundle/lambda_elasticsearch_handler.py
+++ lambda_bundle/lambda_elasticsearch_handler.py
@@ -8,18 +8,29 @@ import urllib
 import json
 import os
 from elasticsearch_wrapper import connectES, indexBulkCsv, createIndex
+from aws_requests_auth.aws_auth import AWSRequestsAuth
+
 
 def index_handler(event, context):
-   esClient = connectES(os.environ['elasticsearch_domain_name'])
-   indexName = "movies"
-   createIndex(esClient, indexName)
+    esdomain = os.environ['elasticsearch_domain_name']
+    auth = AWSRequestsAuth(aws_access_key=os.environ['AWS_ACCESS_KEY_ID'],
+                           aws_secret_access_key=os.environ['AWS_SECRET_ACCESS_KEY'],
+                           aws_token=esdomain,
+                           aws_host=esClient,
+                           aws_region='us-east-1',
+                           aws_service='es')
 
-   # Get the object from the event and show its content type
-   bucket = event['Records'][0]['s3']['bucket']['name']
-   key = urllib.unquote_plus(event['Records'][0]['s3']['object']['key'].encode('utf8'))
-   try:
-     indexBulkCsv(esClient, indexName, bucket, key)
-   except Exception as e:
-     print(e)
-     print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
-     raise e
+    esClient = connectES(esdomain, auth)
+    indexName = "movies"
+    createIndex(esClient, indexName)
+
+    # Get the object from the event and show its content type
+        bucket = event['Records'][0]['s3']['bucket']['name']
+        key = urllib.unquote_plus(
+            event['Records'][0]['s3']['object']['key'].encode('utf8'))
+        try:
+            indexBulkCsv(esClient, indexName, bucket, key)
+        except Exception as e:
+            print(e)
+            print('Error getting object {} from bucket {}. Make sure they exist and your bucket is in the same region as this function.'.format(key, bucket))
+            raise e

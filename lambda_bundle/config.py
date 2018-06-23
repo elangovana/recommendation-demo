be9e058add95b90019de716d0171169215ac8645
aeg <aeg@amazon.com>
2018-06-23 14:20:35 +1000

    add ratings index

--- lambda_bundle/config.py
+++ lambda_bundle/config.py
@@ -1,9 +1,11 @@
 DATASETID_100K = "100KDS"
 DOCTYPE_MOVIES ="movies"
 DOCTYPE_USERS="users"
+DOCTYPE_RATINGS="ratings"
 CSVFIELD_NAMES="fieldnames"
 NB_USERS="nbusers"
 NB_MOVIES="nbMovies"
+
 
 DataSet = {
     DATASETID_100K: {"name": "100K Data set", NB_USERS: 943, NB_MOVIES: 1682,
@@ -12,5 +14,8 @@ DataSet = {
                                             "Childrens", "Comedy", "Crime", "Documentary", "Drama", "Fantasy",
                                             "FilmNoir", "Horror", "Musical", "Mystery", "Romance", "SciFi", "Thriller",
                                             "War", "Western"] }
-                     ,DOCTYPE_USERS:{CSVFIELD_NAMES: ["_id", "age", "gender", "occupation", "zip code"]}}
+                     ,DOCTYPE_USERS:{CSVFIELD_NAMES: ["_id", "age", "gender", "occupation", "zip code"]}
+                     , DOCTYPE_RATINGS:{CSVFIELD_NAMES: ["userid" , "movieid" , "rating", "timestamp"]}
+
+                     }
 }

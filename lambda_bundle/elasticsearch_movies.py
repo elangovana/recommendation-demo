faffe8feb249ce7ba33af1dd3f4178385539341e
aeg <aeg@amazon.com>
2018-06-24 20:23:50 +1000

    fix user

--- lambda_bundle/elasticsearch_movies.py
+++ lambda_bundle/elasticsearch_movies.py
@@ -33,6 +33,7 @@ def get_user_by_id(esClient, id, dataset_id):
     user_search_result = search(esClient, indexName, query, 1)
     print(user_search_result)
     user = user_search_result["hits"]["hits"][0]["_source"]
+    print(user)
     # ratings_query= {"userid": {
     #     "type": config.DOCTYPE_RATINGS,
     #     "values": [user.]

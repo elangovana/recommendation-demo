865b9e5753eee1cb4ba371de24842cce33d31450
aeg <aeg@amazon.com>
2018-06-26 16:01:06 +1000

    Fix comments

--- lambda_bundle/tests/test_lambda_handler.py
+++ lambda_bundle/tests/test_lambda_handler.py
@@ -5,13 +5,14 @@ import lambda_sagemaker_handler
 class TestLambda_handler(TestCase):
 
     def test_recommend(self):
-        # os.environ['sagemaker_endpoint']="factorization-machines-2018-06-19-06-24-41-438"
-        # os.environ['elasticsearch_domain_name'] = 'search-movies-rriuwfsv6q45i6ltiaidxbafky.us-east-2.es.amazonaws.com'
-        # os.environ['AWS_REGION'] = 'us-east-2'
+        #Please set these variables environment variables before running this set, in your python virtual environment
         #
-        # os.environ['AWS_ACCESS_KEY_ID'] = "***"
-        # os.environ['AWS_SECRET_ACCESS_KEY']="******"
-        # os.environ['AWS_SESSION_TOKEN']=""
+        # export sagemaker_endpoint="factorization-machines-2018-06-19-06-24-41-438"
+        # export elasticsearch_domain_name  = 'your-elastic-search-endpoint.us-east-2.es.amazonaws.com'
+        # export AWS_REGION = 'us-east-2'
+        # export AWS_ACCESS_KEY_ID = "***"
+        # export AWS_SECRET_ACCESS_KEY ="******"
+        # export AWS_SESSION_TOKEN =""
 
         lambda_sagemaker_handler.lambda_handler({
             "params": {"querystring":{"userid":90, "dataset_id":"100KDS"}}
